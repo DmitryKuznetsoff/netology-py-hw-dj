e81928faad09939a78477b622ccd09bf836b27bb
DmitryKuznetsoff <kuznetsoffd90@gmail.com>
2020-11-23 00:24:04 +0300

    dj13_diploma fixtures, tests for products endpoint

--- dj13_diploma/tests/api/conftest.py
+++ dj13_diploma/tests/api/conftest.py
@@ -2,12 +2,44 @@ from random import randint
 
 import pytest
 from model_bakery import baker
+from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
 
 
 @pytest.fixture
-def api_client():
-    return APIClient()
+def user(django_user_model):
+    return django_user_model.objects.create(username='user', password='something')
+
+
+@pytest.fixture
+def user_token(user):
+    token, _ = Token.objects.get_or_create(user=user)
+    return token.key
+
+
+@pytest.fixture
+def user_api_client(user_token):
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION=f'Token {user_token}')
+    return client
+
+
+@pytest.fixture
+def admin(django_user_model):
+    return django_user_model.objects.create(username='admin', password='something', is_staff=True)
+
+
+@pytest.fixture
+def admin_token(admin):
+    token, _ = Token.objects.get_or_create(user=admin)
+    return token.key
+
+
+@pytest.fixture
+def admin_api_client(admin_token):
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION=f'Token {admin_token}')
+    return client
 
 
 @pytest.fixture
@@ -16,3 +48,12 @@ def product_factory():
         return baker.make('Product', _quantity=randint(min_amount, max_amount), **kwargs)
 
     return factory
+
+
+@pytest.fixture
+def product_create_payload():
+    return {
+        'name': 'test',
+        'description': 'test',
+        'price': 100
+    }
--- dj13_diploma/tests/api/test_products.py
+++ dj13_diploma/tests/api/test_products.py
@@ -1,16 +1,16 @@
 import random
 
-from django.urls import reverse
 import pytest
-from rest_framework.status import HTTP_200_OK
+from django.urls import reverse
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_403_FORBIDDEN, HTTP_204_NO_CONTENT
 
 
 @pytest.mark.django_db
-def test_course_retrieve(api_client, product_factory):
+def test_product_retrieve(user_api_client, product_factory):
     product = product_factory(min_amount=1, max_amount=1)[0]
     url = reverse('products-detail', args=[product.id])
 
-    resp = api_client.get(url)
+    resp = user_api_client.get(url)
     resp_json = resp.json()
 
     assert resp.status_code == HTTP_200_OK
@@ -18,11 +18,11 @@ def test_course_retrieve(api_client, product_factory):
 
 
 @pytest.mark.django_db
-def test_course_list(api_client, product_factory):
+def test_products_list(user_api_client, product_factory):
     products_list = product_factory()
     url = reverse('products-list')
 
-    resp = api_client.get(url)
+    resp = user_api_client.get(url)
     resp_json = resp.json()
 
     expected_ids = {product.id for product in products_list}
@@ -30,3 +30,117 @@ def test_course_list(api_client, product_factory):
 
     assert resp.status_code == HTTP_200_OK
     assert response_ids == expected_ids
+
+
+@pytest.mark.django_db
+def test_products_filter_by_price(user_api_client, product_factory):
+    random_product_price = random.choice(product_factory()).price
+    url = reverse('products-list')
+
+    resp = user_api_client.get(url, {'price_min': random_product_price, 'price_max': random_product_price})
+    resp_json = resp.json()
+    prices = {product['price'] for product in resp_json}
+    check_price = set(filter(lambda x: x == random_product_price, prices))
+
+    assert resp.status_code == HTTP_200_OK
+    assert prices == check_price
+
+
+@pytest.mark.django_db
+def test_products_filter_by_name(user_api_client, product_factory):
+    random_product_name = random.choice(product_factory()).name
+    url = reverse('products-list')
+
+    resp = user_api_client.get(url, {'name': random_product_name})
+    resp_json = resp.json()
+    names = {product['name'] for product in resp_json}
+    check_names = set(filter(lambda x: x == random_product_name, names))
+
+    assert resp.status_code == HTTP_200_OK
+    assert names == check_names
+
+
+@pytest.mark.django_db
+def test_products_filter_by_description(user_api_client, product_factory):
+    random_product_description = random.choice(product_factory()).description
+    url = reverse('products-list')
+
+    resp = user_api_client.get(url, {'description': random_product_description})
+    resp_json = resp.json()
+    descriptions = {product['description'] for product in resp_json}
+    check_descriptions = set(filter(lambda x: x == random_product_description, descriptions))
+
+    assert resp.status_code == HTTP_200_OK
+    assert descriptions == check_descriptions
+
+
+@pytest.mark.django_db
+def test_products_create_by_user(product_factory, user_api_client, product_create_payload):
+    url = reverse('products-list')
+
+    resp = user_api_client.post(url, data=product_create_payload, format='json')
+
+    assert resp.status_code == HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_products_create_by_admin(product_factory, admin_api_client, product_create_payload):
+    url = reverse('products-list')
+
+    resp = admin_api_client.post(url, data=product_create_payload, format='json')
+    resp_json = resp.json()
+
+    assert resp.status_code == HTTP_201_CREATED
+    assert resp_json['name'] == product_create_payload['name']
+
+
+@pytest.mark.django_db
+def test_products_update_by_user(product_factory, user_api_client):
+    product = product_factory(min_amount=1, max_amount=1, name='product')[0]
+
+    url = reverse('products-detail', args=[product.id])
+    payload = {
+        'name': f'test_{product.name}'
+    }
+
+    resp = user_api_client.patch(url, data=payload)
+
+    assert resp.status_code == HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_products_update_by_admin(product_factory, admin_api_client):
+    product = product_factory(min_amount=1, max_amount=1, name='product')[0]
+
+    url = reverse('products-detail', args=[product.id])
+    payload = {
+        'name': f'test_{product.name}'
+    }
+
+    resp = admin_api_client.patch(url, data=payload)
+    resp_json = resp.json()
+
+    assert resp.status_code == HTTP_200_OK
+    assert payload['name'] == resp_json['name']
+
+
+@pytest.mark.django_db
+def test_products_delete_by_user(product_factory, user_api_client):
+    random_product = random.choice(product_factory())
+    url = reverse('products-detail', args=[random_product.id])
+
+    resp = user_api_client.delete(url)
+
+    assert resp.status_code == HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_products_delete_by_admin(product_factory, admin_api_client):
+    random_product = random.choice(product_factory())
+    url = reverse('products-detail', args=[random_product.id])
+
+    resp = admin_api_client.delete(url)
+    existing_ids = [product['id'] for product in admin_api_client.get(reverse('products-list')).json()]
+
+    assert resp.status_code == HTTP_204_NO_CONTENT
+    assert random_product.id not in existing_ids
